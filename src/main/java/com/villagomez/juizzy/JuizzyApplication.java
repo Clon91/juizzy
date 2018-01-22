05de7f1e14d8d64274e799e9e051f0df9fa87fff
Rodrigo Villagómez <villagomezrios.91@gmail.com>
2018-01-21 18:56:53 -0600

    Estructura inicial proyecto JUIZZY

---
+++ src/main/java/com/villagomez/juizzy/JuizzyApplication.java
@@ -0,0 +1,12 @@
+package com.villagomez.juizzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuizzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuizzyApplication.class, args);
+	}
+}
