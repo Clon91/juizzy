ea515e9faff3afea62833cbbdf1309ed2178e746
Rodrigo Villagómez <villagomezrios.91@gmail.com>
2018-02-06 22:07:08 -0600

    Se inyectan al controlador datos simple, objetos y una lista de objetos

--- src/main/java/com/villagomez/juizzy/controller/HelloRodrigoController.java
+++ src/main/java/com/villagomez/juizzy/controller/HelloRodrigoController.java
@@ -1,10 +1,15 @@
 package com.villagomez.juizzy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.villagomez.juizzy.constants.*;
+
+import com.villagomez.juizzy.constants.Constants;
+import com.villagomez.juizzy.model.Person;
 
 @Controller
 @RequestMapping("/example")
@@ -12,7 +17,19 @@ public class HelloRodrigoController {
 	
 	@GetMapping("/MAV")
 	public ModelAndView exampleMAV() {
-		return new ModelAndView(Constants.VIEW_EXAMPLE);
+		ModelAndView mav = new ModelAndView(Constants.VIEW_EXAMPLE);
+		mav.addObject("name", "Diana");
+		mav.addObject("person", new Person("Rodrigo", 26));
+		mav.addObject("people", getPeople());
+		return mav;
+	}
+	
+	private List<Person> getPeople(){
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Diana", 2));
+		people.add(new Person("Rodrigo", 7));
+		people.add(new Person("Ivana", 3));
+		return people;
 	}
 
 }
